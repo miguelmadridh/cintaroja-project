93d84d59e485d8d6f608162b14effb269a1d9683
Miguel Madrid Hernandez <miguel.madridh@gmail.com>
2020-08-13 18:04:31 -0500

    add docs

---
+++ docs/precache-manifest.121d1bc4dbfb366577e6fded1fc13569.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b53bedc58bd80a0733d54a34a0bdbad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85ef72fc03b3d73c8d0b",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "73be73bbef36c714ee8b",
+    "url": "/static/css/main.d4468b74.chunk.css"
+  },
+  {
+    "revision": "85ef72fc03b3d73c8d0b",
+    "url": "/static/js/2.a7d02551.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.a7d02551.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73be73bbef36c714ee8b",
+    "url": "/static/js/main.c382c4b7.chunk.js"
+  },
+  {
+    "revision": "de8162ede847d85c33a9",
+    "url": "/static/js/runtime-main.f9add001.js"
+  },
+  {
+    "revision": "59b06d6e27edcce4e0d345fdc1e4f1f2",
+    "url": "/static/media/logo.59b06d6e.png"
+  }
+]);
